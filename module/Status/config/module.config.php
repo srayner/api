22742bf5587281367a22e9cb9b305033ad5958cf
Steve Rayner <srayner02@gmail.com>
2016-07-16 22:20:16 +0100

    Inital commit.

---
+++ module/Status/config/module.config.php
@@ -0,0 +1,70 @@
+<?php
+return array(
+    'controllers' => array(
+        'factories' => array(
+            'Status\\V1\\Rpc\\Ping\\Controller' => 'Status\\V1\\Rpc\\Ping\\PingControllerFactory',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'status.rpc.ping' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/ping',
+                    'defaults' => array(
+                        'controller' => 'Status\\V1\\Rpc\\Ping\\Controller',
+                        'action' => 'ping',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'zf-versioning' => array(
+        'uri' => array(
+            0 => 'status.rpc.ping',
+        ),
+    ),
+    'zf-rpc' => array(
+        'Status\\V1\\Rpc\\Ping\\Controller' => array(
+            'service_name' => 'Ping',
+            'http_methods' => array(
+                0 => 'GET',
+            ),
+            'route_name' => 'status.rpc.ping',
+        ),
+    ),
+    'zf-content-negotiation' => array(
+        'controllers' => array(
+            'Status\\V1\\Rpc\\Ping\\Controller' => 'Json',
+        ),
+        'accept_whitelist' => array(
+            'Status\\V1\\Rpc\\Ping\\Controller' => array(
+                0 => 'application/vnd.status.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ),
+        ),
+        'content_type_whitelist' => array(
+            'Status\\V1\\Rpc\\Ping\\Controller' => array(
+                0 => 'application/vnd.status.v1+json',
+                1 => 'application/json',
+            ),
+        ),
+    ),
+    'zf-content-validation' => array(
+        'Status\\V1\\Rpc\\Ping\\Controller' => array(
+            'input_filter' => 'Status\\V1\\Rpc\\Ping\\Validator',
+        ),
+    ),
+    'input_filter_specs' => array(
+        'Status\\V1\\Rpc\\Ping\\Validator' => array(
+            0 => array(
+                'required' => true,
+                'validators' => array(),
+                'filters' => array(),
+                'name' => 'ack',
+                'description' => 'Acknowledge the request with a timestamp',
+            ),
+        ),
+    ),
+);
